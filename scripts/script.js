0aa99a8ec7dae7e7ad3eb23ca3392607a90fbdad
Yue Zhuo <y1zhuo@ucsd.edu>
2021-05-23 13:18:53 -0700

    elint for script

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,66 @@
 // script.js
 
- /****************NAVIGATION********************/ 
+/** *************NAVIGATION******************* */
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 
-let left_arrow = document.getElementById('left-arrow');
-left_arrow.onclick = function (){
+let leftArrow = document.getElementById('left-arrow');
+leftArrow.onclick = function (){
   // get title
   // determine state based on title
   setState({page: "Settings", day: "title"});
 }
 
-let right_arrow = document.getElementById('right-arrow');
-right_arrow.onclick = function (){
-  //setState();
+let rightArrow = document.getElementById('right-arrow');
+rightArrow.onclick = function (){
+  // setState();
 }
 
-let daily_tab = document.getElementById('daily-btn');
-daily_tab.onclick = function (){
+const dailyTab = document.getElementById('daily-btn');
+dailyTab.onclick = function (){
     // for element in localstorage.daily{
     //     daily_tab.appendChild(html.a)
     // }
 }
 
-let weekly_tab = document.getElementById('weekly-btn');
-weekly_tab.onclick = function (){
-  //setState();
+let weeklyTab = document.getElementById('weekly-btn');
+weeklyTab.onclick = function (){
+  // setState();
 }
 
-let monthly_tab = document.getElementById('monthly-btn');
-monthly_tab = function (){
-  //setState();
+let monthlyTab = document.getElementById('monthly-btn');
+monthlyTab = function (){
+  // setState();
 }
 
-let future_tab = document.getElementById('future-btn');
-future_tab.onclick = function (){
-  //setState();
+let futureTab = document.getElementById('future-btn');
+futureTab.onclick = function (){
+  // setState();
 }
 
 
-let home_button = document.getElementById('flamingo-logo');
-//  go to the index/home page
-home_button.onclick = function (){
-  //setState({page: 'index'});
+let homeButton = document.getElementById('flamingo-logo');
+// go to the index/home page
+homeButton.onclick = function (){
+  // setState({page: 'index'});
 }
 
 
 /****************FUNCTIONS********************/
 
 // Collapsing SideBar methods NOT NEEDED
-let sidebar_button = document.getElementById('sidebar-button');
+let sidebarButton = document.getElementById('sidebar-button');
 let sidebar = document.getElementById('sidebar');
-sidebar_button.onclick = function () {
+sidebarButton.onclick = function () {
     console.log("works");
     if (sidebar.className == "sidebaron") {
         sidebar.className = "sidebaroff";  
-        sidebar_button.className = 'open';
-        sidebar_button.value = '>';
+        sidebarButton.className = 'open';
+        sidebarButton.value = '>';
     }else {
         sidebar.className = "sidebaron";
-        sidebar_button.className = 'collapse';
-        sidebar_button.value = '<';
+        sidebarButton.className = 'collapse';
+        sidebarButton.value = '<';
     }
     
 }
