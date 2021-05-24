a35a82f6943b3e18ab71cdc13ddb156b65c2fb97
stalaver <stalaver@ucsd.edu>
2021-05-24 14:26:58 -0700

    Setting only scripts directory

---
+++ scripts/jsdoc_test.js
@@ -0,0 +1,21 @@
+
+/**
+ * Test Name
+ * @type {string}
+ */
+const testName = 'Joe';
+
+/**
+ * Test array of numbers
+ * @type {Array<number>}
+ */
+const numbers = [55, 3, 22];
+
+/**
+ * Test object
+ * @type {{objNum: number, objText: string}}
+ */
+const obj = {
+    objNum: 5,
+    objText: 'Test'
+}
