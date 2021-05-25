29fd0bbc2f305eaa8da9ea04c608df15ee786e46
Yue Zhuo <y1zhuo@ucsd.edu>
2021-05-24 22:20:45 -0700

    change key.js

--- scripts/key.js
+++ scripts/key.js
@@ -4,27 +4,15 @@ let area;
 
 // Get text area when clicked
 textAreas.forEach((textArea) => {
-<<<<<<< HEAD
     textArea.addEventListener('click', () => {
         area = textArea;
         // console.log(area);
     })
-=======
-  textArea.addEventListener('click', () => {
-    area = textArea
-    // console.log(area);
-  })
->>>>>>> b1a612d91892de66d5b3fc8eb5388d1d048561b5
 })
 
 // on button click add key to last clicked textarea
 btns.forEach((btn) => {
-<<<<<<< HEAD
     btn.addEventListener('click', () => {
         area.value += btn.value;
-=======
-  btn.addEventListener('click', () => {
-    area.value += btn.value
->>>>>>> b1a612d91892de66d5b3fc8eb5388d1d048561b5
     })
 })
