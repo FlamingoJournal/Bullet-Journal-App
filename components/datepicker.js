de4e8465609f3ade14619f6231311d378336c2d6
stalaver <stalaver@ucsd.edu>
2021-05-25 15:23:14 -0700

    Updated datepicker

---
+++ components/datepicker.js
@@ -0,0 +1,18 @@
+// Datapicker.js
+/**
+ * Function that clears the selected date on the datepicker
+ */
+const clearBtn = document.getElementById('clear-btn')
+clearBtn.onclick = function clear() {
+    document.getElementById('calendar').valueAsDate = null
+}
+
+/**
+ * Function that searches the picked date on the datepicker
+ * @todo Get month and year from outputted string
+ */
+const searchBtn = document.getElementById('search-btn')
+searchBtn.onclick = function search() {
+    const selDate = document.getElementById('calendar').value
+    console.log(selDate)
+}
