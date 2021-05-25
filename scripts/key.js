8e2f523fbd710d5295a70ef347b25b3f35cd822c
Yue Zhuo <y1zhuo@ucsd.edu>
2021-05-24 21:56:39 -0700

    key-edits

---
+++ scripts/key.js
@@ -0,0 +1,18 @@
+const btns = document.getElementById("button-group").querySelectorAll('button'); // keys
+const textAreas = document.querySelectorAll("textarea");
+let area;
+
+// Get text area when clicked
+textAreas.forEach((textArea) => {
+    textArea.addEventListener('click', () => {
+        area = textArea;
+        // console.log(area);
+    });
+});
+
+// on button click add key to last clicked textarea
+btns.forEach((btn) => {
+       btn.addEventListener('click', () => {
+        area.value += btn.value;
+       }); 
+});
